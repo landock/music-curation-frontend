af8e47f32ba0b77af32c54c8b8a1daecf700d048
Michael Easley <michael.easley@iamplus.com>
2018-05-22 10:19:48 -0700

    Added eslint;

--- src/stories/index.js
+++ src/stories/index.js
@@ -15,25 +15,20 @@ import 'rxjs/add/operator/first';
 
 import 'semantic-ui-css/semantic.min.css';
 
-import '../components/App/App.story';
-
 import '../components/CurationsCollection/CurationsCollection.story';
 import '../components/Curation/Curation.story';
-import '../components/CurationScheduler/CurationScheduler.story';
-
-import '../components/EditCuration/EditCuration.story';
 import '../components/PublicationStatusEditor/PublicationStatusEditor.story';
 import '../components/ModifyCuration/ModifyCuration.story';
-
+import '../components/EditCuration/EditCuration.story';
+import '../components/CurationScheduler/CurationScheduler.story';
 import '../components/TagCollection/TagCollection.story';
 import '../components/Tag/Tag.story';
-
 import '../components/SearchResultCollection/SearchResultCollection.story';
 import '../components/SearchResultCard/SearchResultCard.story';
 import '../components/AddTags/AddTags.story';
 import '../components/AddToCuration/AddToCuration.story';
-
 import '../components/CurationSearchBar/CurationSearchBar.story';
 import '../components/CurationFilterSubHeader/CurationFilterSubHeader.story';
 import '../components/SearchResultFilterSubHeader/SearchResultFilterSubHeader.story';
 import '../components/BulkCurationEditor/BulkCurationEditor.story';
+import '../components/App/App.story';
